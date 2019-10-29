f963f0407a8854582096b17c00fb008bac50e880
Wisnu Widjo <wisnuwijo33@gmail.com>
2019-10-29 07:53:40 +0700

    Add support for #, & and / sign

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,12 @@
+## [0.0.3] - 29 October 2019.
+
+* Update README.md
+* Add example
+
 ## [0.0.2] - 29 October 2019.
 
 * Add support for #, & and / sign.
 
 ## [0.0.1] - 28 October 2019.
 
-* initial release.
+* Initial release.
--- example/main.dart
+++ example/main.dart
@@ -1,15 +1,3 @@
-# url_encoder
-
-Encode url breaker character (/, &, #) to UTF-8. 
-Helpful package to avoid url break request
-
-## Getting Started
-
-To use this plugin, add `url_encoder` as a [dependency in your pubspec.yaml file](https://flutter.io/platform-plugins/).  
-
-### Example  
-  
-```dart  
 import 'package:flutter/material.dart';  
 import 'package:url_encoder/url_encoder.dart';  
   
@@ -41,4 +29,3 @@ class SplashTapDemo extends StatelessWidget {
     );  
   }  
 }  
-```
